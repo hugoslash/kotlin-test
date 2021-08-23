8c310a304dcf1d68640947625f3d97ab7f905846
Hugo Slash <hugo.perez@slashmobility.com>
2021-08-23 16:43:27 +0200

    feat: First Unit Test implemented

--- src/test/kotlin/com/slashmobility/kotlinbasebackend/KotlinBaseBackendApplicationTests.kt
+++ src/test/kotlin/com/slashmobility/kotlinbasebackend/KotlinBaseBackendApplicationTests.kt
@@ -1,13 +1,20 @@
 package com.slashmobility.kotlinbasebackend
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
-class KotlinBaseBackendApplicationTests {
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+class KotlinBaseBackendApplicationTests(@Autowired val client : TestRestTemplate) {
 
 	@Test
-	fun contextLoads() {
+	fun healthEndpoint() {
+		val hello = client.getForEntity<String>("/api/health")
+		assertThat(hello.statusCode).isEqualTo(HttpStatus.OK)
 	}
 
 }
